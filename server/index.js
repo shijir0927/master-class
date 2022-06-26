5b459925dd55249abbbe53a224273a80527936f3
shijir0927 <shijiree_mng@yahoo.com>
2022-06-25 19:32:40 -0700

    connect express route to react

--- server/index.js
+++ server/index.js
@@ -8,10 +8,16 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hi Shijir!!!" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
